b11f7338e73d73b0f289600334c2401d9b4befe1
RevolutionAdmin <achyut.kendre@gmail.com>
2026-06-08 09:09:37 +0530

    this is demo controller

---
+++ CURDUsingAzureSQLDB/Controllers/DemoAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CURDUsingAzureSQLDB.Controllers
+{
+    public class DemoAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
